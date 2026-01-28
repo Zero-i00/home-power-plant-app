1aca560adc9377b787576df306e6e282e29ec873
dedshifu <laptevzahar984@gmail.com>
2026-01-28 22:12:43 +0500

    create wind generation

--- services/calculation.py
+++ services/calculation.py
@@ -6,7 +6,7 @@ from models.city import MonthDaysEnum
 class Calculation:
 
     @staticmethod
-    async def solar_station(session: AsyncSession, city_id: int, panel_power: float, panel_kpd: float) -> float:
+    async def solar_station(session: AsyncSession, city_id: int, panel_power: float, sist_kpd: float, price_energy_sun: float, price_sun: float ) -> float:
         city = await city_service.retrieve(session, city_id)
 
         if city is None:
@@ -27,16 +27,39 @@ class Calculation:
 
         for param in parameters:
             day_peer_month = MonthDaysEnum[param.month.name]
-            total += param.insolation * panel_power * panel_kpd * day_peer_month
+            total += param.insolation * panel_power  *  day_peer_month * sist_kpd
 
-        return total
+        result =   price_sun / (total * price_energy_sun) 
+
+        return result
+
+
     
-    # TODO написать функцию вычислений для генератора ветра
-    # session: AsyncSession, city_id: int нужно через , дополнить параметрами из CalculationWindGeneratorIn 
     @staticmethod
-    async def wind_generator(session: AsyncSession, city_id: int) -> float:
-        #  Копируешь всё из solar_station и меняешь только вычисления в for
-        ...
+    async def wind_generator(session: AsyncSession, city_id: int, blade_length: float) -> float:
+        city = await city_service.retrieve(session, city_id)
+
+        if city is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Не удалось найти такой город'
+            )
+    
+        parameters = await city_parameters_service.list_by_city_id(session, city.id)
+
+        if parameters is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'Не удалось найти параметры для города {city.name}'
+            )
+        
+        total1 = 0
+
+        for param in parameters:
+            day_peer_month = MonthDaysEnum[param.month.name]
+            total1 += 0.5 * param.air_density * ((blade_length * blade_length) * 3.14)  * (param.wind_speed*param.wind_speed*param.wind_speed) * 0.3 * 24 * day_peer_month * 0.75 
+
+        return total1
 
     
 calculation_service = Calculation()
